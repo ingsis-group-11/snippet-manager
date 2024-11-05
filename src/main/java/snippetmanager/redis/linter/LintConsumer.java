514121a5a48de55dc0e6842f6fc978fcf0f1c458
Tomás Serra <tomi.serra@gmail.com>
2024-11-05 14:54:27 -0300

    Consumer

--- src/main/java/snippetmanager/redis/linter/LintConsumer.java
+++ src/main/java/snippetmanager/redis/linter/LintConsumer.java
@@ -1,58 +1,51 @@
-package snippetmanager.redis.linter;
+package printscriptservice.redis;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.PostConstruct;
+import java.time.Duration;
+import org.austral.ingsis.redis.RedisStreamConsumer;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.connection.stream.MapRecord;
-import org.springframework.data.redis.connection.stream.StreamOffset;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.stream.StreamReceiver;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
+import snippetmanager.redis.linter.LinterRedisResult;
 import snippetmanager.services.LintingRuleService;
 
 @Component
-public class LintConsumer {
+public class LintConsumer extends RedisStreamConsumer<String> {
 
-  private final ReactiveRedisTemplate<String, String> redisTemplate;
-  private final StreamReceiver<String, MapRecord<String, String, String>> streamReceiver;
-  private final String streamKey;
+  @Autowired private LintingRuleService lintingRuleService;
 
-  @Autowired LintingRuleService lintingRuleService;
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
   public LintConsumer(
-      @Value("${redis.consumer.lint}") String streamKey,
-      ReactiveRedisTemplate<String, String> redisTemplate,
-      StreamReceiver<String, MapRecord<String, String, String>> streamReceiver) {
-    this.streamKey = streamKey;
-    this.redisTemplate = redisTemplate;
-    this.streamReceiver = streamReceiver;
+          @Value("${redis.consumer.lint}") @NotNull String streamKey,
+          @Value("${redis.consumer.group}") @NotNull String consumerGroup,
+          @NotNull RedisTemplate<String, String> redis) {
+    super(streamKey, consumerGroup, redis);
   }
 
-  @PostConstruct
-  public void startConsuming() {
-    Flux<MapRecord<String, String, String>> messageFlux =
-        streamReceiver.receive(StreamOffset.fromStart(streamKey));
-
-    messageFlux.doOnNext(this::processMessage).subscribe();
-  }
-
-  private void processMessage(MapRecord<String, String, String> message) {
-
-    String messageBody = message.getValue().get("payload");
-
+  @Override
+  protected void onMessage(@NotNull ObjectRecord<String, String> objectRecord) {
     try {
-      ObjectMapper objectMapper = new ObjectMapper();
-      JsonNode jsonNode = objectMapper.readTree(messageBody);
+      String jsonValue = objectRecord.getValue();
+      LinterRedisResult linterRedisResult =
+              objectMapper.readValue(jsonValue, LinterRedisResult.class);
 
-      String assetId = jsonNode.get("assetId").asText();
-      String result = jsonNode.get("result").asText();
-
-      lintingRuleService.saveLintResult(assetId, result);
+      lintingRuleService.saveLintResult(linterRedisResult.getAssetId(), linterRedisResult.getLinterResult());
     } catch (Exception e) {
-      throw new RuntimeException("Error processing message", e);
+      throw new RuntimeException(e);
     }
+  }
+
+  @NotNull
+  @Override
+  protected StreamReceiver.StreamReceiverOptions<String, ObjectRecord<String, String>> options() {
+    return StreamReceiver.StreamReceiverOptions.builder()
+            .pollTimeout(Duration.ofSeconds(5))
+            .targetType(String.class)
+            .build();
   }
 }
