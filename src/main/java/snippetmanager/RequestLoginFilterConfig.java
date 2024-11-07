7c2738b1c288316c0c79e8031a122a2273577038
franciscolang <francisco.lang2209@gmail.com>
2024-11-07 17:20:04 -0300

    Added logging to snippet requests

---
+++ src/main/java/snippetmanager/RequestLoginFilterConfig.java
@@ -0,0 +1,19 @@
+package snippetmanager;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoginFilterConfig {
+
+  @Bean
+  public CommonsRequestLoggingFilter logFilter() {
+    CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+    filter.setIncludeQueryString(true);
+    filter.setIncludePayload(false);
+    filter.setIncludeHeaders(false);
+    filter.setAfterMessagePrefix("REQUEST DATA : ");
+    return filter;
+  }
+}
